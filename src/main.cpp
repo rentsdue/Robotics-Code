6d3a3500eedb7dc045f3abb7db28d449a6a3dc7c
rentsdue <hoganwong88@gmail.com>
2023-12-01 12:44:32 +0800

    e

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
@@ -19,32 +18,33 @@
 // catapult             motor         3              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
-
 #include "vex.h"
 #include "robot-config.h"
 
-
 using namespace vex;
 
-
 int main() {
- // Initializing Robot Configuration. DO NOT REMOVE!
- vexcodeInit();
- while(1){
-   front_left.spin(forward,Controller1.Axis3.position(percent),percent);
-   // front_leftモータの速度ををコントローラーのAxis3の位置(パーセント)によって動かす
-   back_left.spin(reverse,Controller1.Axis3.position(percent),percent);
-   front_right.spin(forward,Controller1.Axis2.position(percent),percent);
-   back_right.spin(reverse,Controller1.Axis2.position(percent),percent);
+    // Initializing Robot Configuration. DO NOT REMOVE!
+    vexcodeInit();
 
+    while (true) {
+        // Drive control
+        front_left.spin(forward, Controller1.Axis3.position(percent), percent);
+        back_left.spin(reverse, Controller1.Axis3.position(percent), percent);
+        front_right.spin(forward, Controller1.Axis2.position(percent), percent);
+        back_right.spin(reverse, Controller1.Axis2.position(percent), percent);
 
-   // catapult
-   if(Controller1.ButtonL1.pressing()){//
-     catapult.spin(reverse);
-   }else{catapult.stop();}
- }
- front_left.stop();
- back_left.stop();
- front_right.stop();
- back_right.stop();
+        // Catapult control
+        if (Controller1.ButtonL1.pressing()) {
+            catapult.spin(reverse);
+        } else {
+            catapult.stop();
+        }
+    }
+
+    // Ensuring motors are stopped at the end of the program
+    front_left.stop();
+    back_left.stop();
+    front_right.stop();
+    back_right.stop();
 }
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -7,7 +7,7 @@ using code = vision::code;
 
 
 // A global instance of brain used for printing to the V5 Brain screen
-brain  Brain;
+brain Brain;
 
 
 // VEXcode device constructors
